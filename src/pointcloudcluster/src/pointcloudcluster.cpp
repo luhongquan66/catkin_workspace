15eea38d99562380ac9d3e5e0013fa7b8a362a7c
luhongquan <luhongquan66@163.com>
2019-04-04 16:00:19 +0800

    添加缺失文件

--- src/pointcloudcluster/src/pointcloudcluster.cpp
+++ src/pointcloudcluster/src/pointcloudcluster.cpp
@@ -1,11 +1,12 @@
 #include <pointcloudcluster/pointcloudcluster.hpp>
 #include <slam_common/benchmarker.hpp>
 
+
 PointCloudCluster::PointCloudCluster(ros::NodeHandle nh)
     : nh_(nh)
 {
     // Define Publishers and Subscribers here
-    pcl_sub_ = nh_.subscribe("/velodyne_points", 1, &PointCloudCluster::scanCallback, this);
+    pcl_sub_ = nh_.subscribe("/neuavslam/groudPointCloud", 1, &PointCloudCluster::scanCallback, this);
     pcl_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/cluster_point_cloud", 1);
 
 
@@ -16,7 +17,14 @@ PointCloudCluster::~PointCloudCluster(){}
 
 void PointCloudCluster::scanCallback(const sensor_msgs::PointCloud2ConstPtr& cloudMessIN)
 {
+    pcl::PointCloud<pcl::PointXYZ> cloudIn;
 
+    pcl::fromROSMsg(*cloudMessIN,cloudIn);
+
+    for(size_t index = 0 ; index < 14; index++)
+    {
+        ROS_INFO("%f, %f, %f \n",cloudIn.points[index].x,cloudIn.points[index].y,cloudIn.points[index].z);
+    }
 }
   // End of class CloudTransformer
 
